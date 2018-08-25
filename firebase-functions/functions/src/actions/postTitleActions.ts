8eb042a9c28ba29b9aef585c0f760067db916177
Paul Krisadaolrn <pauly.k@gmail.com>
2018-08-25 19:03:16 +0700

    functions: ensure posttitle/{postId} doc create/updated on post create/update

---
+++ firebase-functions/functions/src/actions/postTitleActions.ts
@@ -0,0 +1,24 @@
+import * as admin from "firebase-admin";
+
+export async function set_postTitle(
+  postId: string,
+  userId: string,
+  title: string
+) {
+  try {
+    const ref = admin.firestore().doc(`posttitle/${postId}`);
+    const postBody = { userId, title };
+    return ref.set(postBody);
+  } catch (err) {
+    return err;
+  }
+}
+
+export async function delete_postTitle(postId: string) {
+  try {
+    return admin.firestore().doc(`posttitle/${postId}`).delete()
+  } catch (err) {
+    return err
+  }
+}
+

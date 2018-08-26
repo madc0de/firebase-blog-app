fc82e9216d9c5ad76030ae960bbfed2180404d0b
Paul Krisadaolrn <pauly.k@gmail.com>
2018-08-26 11:02:42 +0700

    Post Titles

--- client/src/data/Metadata.ts
+++ client/src/data/Metadata.ts
@@ -1,13 +1,12 @@
 // import * as firebase from "firebase/app";
 import { firestore } from "./firestore-init";
-import * as mapUtil from "../utils/mapUtil";
 import { BlogMetaData } from "../interface/BlogMetaData";
 
 export const getMetadata = async (): Promise<BlogMetaData | undefined> => {
   try {
     const snapshot = await firestore.doc(`metadata/values`).get();
     if (snapshot.exists) {
-      return mapUtil.snapshotToMap(snapshot) as BlogMetaData;
+      return snapshot.data() as BlogMetaData;
     }
     return undefined;
   } catch (error) {
---
+++ client/src/data/PostTitles.ts
@@ -0,0 +1,22 @@
+import { firestore } from "./firestore-init";
+import * as mapUtil from "../utils/mapUtil";
+import { PostTitleDocument } from "../interface/PostTitleData";
+
+export const getPostTitles = async (
+  userId: string
+): Promise<PostTitleDocument[]> => {
+  try {
+    const querySnap = await firestore
+      .collection("posttitle")
+      .where("userId", "==", userId)
+      .get();
+    if (querySnap.size > 0) {
+      return querySnap.docs.map(
+        doc => mapUtil.snapshotToMap(doc) as PostTitleDocument
+      );
+    }
+    return [];
+  } catch (error) {
+    return error;
+  }
+};

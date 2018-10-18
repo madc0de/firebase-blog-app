2eb77ee55c5f0995e037759066740840cdcf3aa1
Paul Krisadaolrn <pauly.k@gmail.com>
2018-10-18 17:00:04 +0700

    Header title links to home

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -3,9 +3,9 @@ import SideNav from "./SideNav";
 import { connect, Dispatch } from "react-redux";
 import { AuthState } from "../../interface/AuthState";
 import { AppState } from "../../interface/AppState";
+import { NavLink } from "react-router-dom";
 
-import blogSettings from '../../blogSettings'
-
+import blogSettings from "../../blogSettings";
 
 const OpenNavButton = (props: { onClick: React.EventHandler<any> }) => (
   <a href="#" className="open-nav-button" onClick={props.onClick}>
@@ -43,14 +43,15 @@ class Header extends React.Component<HeaderProps & Dispatch, HeaderState> {
   };
 
   public render() {
-    
     return (
       <React.Fragment>
         <header className="blog-header">
+          <NavLink to="/">
             <h1 className="title">{blogSettings.blog_title}</h1>
-            <h4 className="description">{blogSettings.blog_description}</h4>
+          </NavLink>
+          <h4 className="description">{blogSettings.blog_description}</h4>
 
-            <OpenNavButton onClick={this.showNav} />
+          <OpenNavButton onClick={this.showNav} />
         </header>
         <SideNav
           visible={this.state.navVisible}
